e4fcbf95d68e08bae979fb4dc0efb4b1679d346a
zbynek03 <sev38138@spseol.cz>
2022-01-18 13:24:07 +0100

    18.1

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "milis.h"
 
 /*#include "delay.h"*/
+#include "spse_stm8.h"
 #include <stdio.h>
 #include "../lib/uart.c"
 
@@ -24,23 +25,48 @@ void setup(void)
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     init_milis();
+    ADC_init();
 }
-
 
 int main(void)
 {
     uint32_t time = 0;
-
+    
     setup();
     init_uart();
+    
+    // na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4, DISABLE);
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5, DISABLE);
+
+    // při inicializaci volíme frekvenci AD převodníku mezi 1-4MHz při 3.3V
+    // mezi 1-6MHz při 5V napájení
+    // nastavíme clock pro ADC (16MHz / 4 = 4MHz)
+    ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
+
+    // volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
+    ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
+    
+    // nasatvíme multiplexer na některý ze vstupních kanálů
+    ADC2_Select_Channel(ADC2_CHANNEL_4);
+
+    // rozběhneme AD převodník
+    ADC2_Cmd(ENABLE);
+
+    // počkáme než se AD převodník rozběhne (~7us)
+    ADC2_Startup_Wait();
+
+    int16_t ADCx;
+    int32_t voltage;
 
     while (1) {
 
         if (milis() - time > 333 && BTN_PUSH) {
             LED_TOGG; 
             time = milis();
-
-            printf("ahoj\r\n");
+            ADCx = ADC_get(ADC2_CHANNEL_4);
+            voltage = (uint32_t)3300 * ADCx + 512 / 1024;
+            printf("R=%d U=%ld\n", ADCx, voltage);
         }
 
         /*LED_FLIP; */
